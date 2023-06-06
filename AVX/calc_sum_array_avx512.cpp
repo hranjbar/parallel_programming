0c481295eb6a23b8efe1e7f556be1d6762fe4752
Homayoon Ranjbar <horanj.1022@gmail.com>
2023-06-06 18:22:45 -0400

    added .gitignore file

--- AVX/calc_sum_array_avx512.cpp
+++ AVX/calc_sum_array_avx512.cpp
@@ -14,22 +14,22 @@ int main () {
 	}
 	auto beg = chrono::high_resolution_clock::now();
 
-	__m256i sumx = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 0, 0);
-	__m256i numx;
-	for (unsigned int i = 0; i < SIZE; i += 8)
+	__m512i sumx = _mm512_set_epi32(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	__m512i numx;
+	for (unsigned int i = 0; i < SIZE; i += 16)
 	{
-		numx = _mm256_load_si256((__m256i const *)(nums + i));
-		sumx = _mm256_add_epi32(numx, sumx);
+		numx = _mm512_load_si512((__m512i const *)(nums + i));
+		sumx = _mm512_add_epi32(numx, sumx);
 	}
 
-	int sums[8];
-	_mm256_storeu_si256((__m256i *)sums, sumx);
+	int sums[16];
+//	_mm512_storeu_si512((__m512i *)sums, sumx);
 	int tot = 0;
-	for (unsigned int i = 0; i < 8; i++)
+/*	for (unsigned int i = 0; i < 16; i++)
 	{
 		tot += sums[i];
 	}
-
+*/
 	auto end = chrono::high_resolution_clock::now();
 	auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - beg).count();
 	cout << "AVX total: " << tot << ", " << elapsed << " ms\n";
